7e1e9956856e2f563fb676539a51ea19d7e8212f
Ankur Goel <ankurgel@gmail.com>
2012-10-20 19:34:02 +0530

    basic rails skelton

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'haml-rails'
+gem 'bootstrap-sass', '2.0.4'
+gem 'twitter'
+
+group :development, :test do
+  gem 'pry-rails', :group => :development
+  gem 'rspec-rails', '2.11.0'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'wirb'
+  gem 'awesome_print'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+group :production do
+  gem 'pg', '0.12.2'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwiBubble::Application.config.secret_token = '4ca3ee714d010bafaab7acb2f353a47290ad65d9d113ca969b15e6a13d915f5df7d7e7194adec0dea58bdf2edcb60d38b626dd3554d6220d25eeda5b0e2ab544'
