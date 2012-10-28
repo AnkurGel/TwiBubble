b811512fa48186049184ca38bb5aa3a8da8048e9
Ankur Goel <ankurgel@gmail.com>
2012-10-28 20:10:22 +0530

    Cursoring implemented. Next: MakeUp

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,18 +15,6 @@
 //= require_tree .
 //= require jqcloud
 
-// if (gon.hash)
-//   alert(gon.hash);
-
-
-// var word_array = [
-//   {text: "15", weight: 15},
-//   {text: "9", weight: 9, link: "http://jquery.com/"},
-//   {text: "6", weight: 6, html: {title: "I can haz any html attribute"}},
-//   {text: "7", weight: 7},
-  
-//   {text: "5", weight: 5}
-// ];
 
 var word_array = new Array();
 var tweets = gon.tweets;
--- app/controllers/clouds_controller.rb
+++ app/controllers/clouds_controller.rb
@@ -4,7 +4,18 @@ class CloudsController < ApplicationController
 
   def create
     if params['cloud']
-      @tweets = Twitter.user_timeline(params['cloud']['twitter_handle'], :exclude_replies => true, :count => 200).map(&:text).map{ |y| y.split.count }
+      @tweets = Array.new
+      last_tweet_id = nil
+      until @tweets.count.eql?(500) do
+        unless last_tweet_id.nil?
+          @tweets_round = Twitter.user_timeline(params['cloud']['twitter'], :exclude_replies => true, :count => 100, :max_id => last_tweet_id)
+        else
+          @tweets_round = Twitter.user_timeline(params['cloud']['twitter'], :exclude_replies => true, :count => 100)
+        end
+        last_tweet_id = @tweets_round.last.id
+        @tweets_round.map(&:text).each { |tweet| @tweets.push(tweet.split.count) }
+      end
+#      @tweets = Twitter.user_timeline(params['cloud']['twitter_handle'], :exclude_replies => true, :count => 200).map(&:text).map{ |y| y.split.count }
       @tweets_hash = Hash.new(0)
       if @tweets
         @tweets.each { |tweet| @tweets_hash[tweet] += 1 }
